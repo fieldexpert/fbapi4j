fefb8d8e6050d60c57c88c12955697ab203d933a
Nathan Bowser <nathan.bowser@fieldexpert.com>
2010-03-09 15:54:26 -0500

    Formatting

--- src/com/fieldexpert/fbapi4j/Priority.java
+++ src/com/fieldexpert/fbapi4j/Priority.java
@@ -1,8 +1,13 @@
 package com.fieldexpert.fbapi4j;
 
 public enum Priority {
-	MUST_FIX_1(1), MUST_FIX_2(2), MUST_FIX_3(3), FIX_IF_TIME_4(4), FIX_IF_TIME_5(5), //
-	FIX_IF_TIME_6(6), DO_NOT_FIX(7);
+	MUST_FIX_1(1), //
+	MUST_FIX_2(2), //
+	MUST_FIX_3(3), //
+	FIX_IF_TIME_4(4), //
+	FIX_IF_TIME_5(5), //
+	FIX_IF_TIME_6(6), //
+	DO_NOT_FIX(7);
 
 	private int value;
 
