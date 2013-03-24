cd024d6cf38a6e58090fa7b3159df5c2e937a7f0
unknown <86492C@EURCSQNP20433.AMERICAS.CORP.LOCAL>
2013-03-24 15:16:22 +0000

    Custom Columns: Support for custom columns (AKA fields) on cases for use with third-party plugins, e.g. Kanban.

--- test/com/fieldexpert/fbapi4j/CaseTest.java
+++ test/com/fieldexpert/fbapi4j/CaseTest.java
@@ -1,6 +1,7 @@
 package com.fieldexpert.fbapi4j;
 
 import static junit.framework.Assert.assertEquals;
+import junit.framework.AssertionFailedError;
 
 import org.junit.Test;
 
@@ -13,6 +14,34 @@ public class CaseTest {
 		assertEquals("fbapi4j", bug.getProject());
 		assertEquals("Misc", bug.getArea());
 		assertEquals("Test Case Title", bug.getTitle());
+	}
+	
+	@Test
+	public void customColumnSetter() {
+		Case bug = new Case("fbapi4j", "Misc", "Test Case Title", "Blue Smoke");
+		bug.setField("some-field", "some-value");
+		
+		assertEquals("Custom field getter didn't return value given to setter", "some-value", bug.getField("some-field"));
+	}
+	
+	@Test
+	public void customColumnUnsetGetter() {
+		Case bug = new Case("fbapi4j", "Misc", "Test Case Title", "Blue Smoke");
+		
+		boolean caughtException = false;
+		try {
+			// When we call a getter, without having set the field.....
+			bug.getField("some-field");
+		} catch (IllegalArgumentException ex) {
+			// ... we expect to  get an IAE!
+			caughtException = true;
+		}
+		
+		if (! caughtException) {
+			throw new AssertionFailedError(String.format(
+					"Calling custom column getter on Case should throw %s if column not previously set",
+					IllegalArgumentException.class.getSimpleName()));
+		}
 	}
 
 }
